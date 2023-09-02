16ba59ab95e5f8a5735f3cc74c6b0cdc33e2ad9b
Sudipta Jana <sjgalaxy98@gmail.com>
2023-09-02 14:40:42 +0530

    collection page modification

--- assets/common.js
+++ assets/common.js
@@ -110,22 +110,24 @@ jQuery(document).ready(function ($) {
     ],
   });
 
-  $(".product-details-slider").slick({
-    autoplay: true,
-    autoplaySpeed: 1500,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    dots: false,
-    navs: true,
-    responsive: [
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
+  setTimeout(() => {
+    $(".product-details-slider").slick({
+      autoplay: true,
+      autoplaySpeed: 1500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: false,
+      navs: true,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+          },
         },
-      },
-    ],
-  });
+      ],
+    });
+  }, 2000);
 
   $(".product-thumb-slider").slick({
     slidesToShow: 1,
