5ba14e5e09b8c894e8820813b3a06e3ab63596c5
Sudipta Jana <sjgalaxy98@gmail.com>
2023-09-19 13:33:47 +0530

    page modification

--- assets/common.js
+++ assets/common.js
@@ -168,5 +168,42 @@ jQuery(document).ready(function ($) {
     }
   });
 
+  // Get the current URL
+  var currentUrl = window.location.href;
+
+  // Define the URL of your home page
+  var homePageUrl = "https://cobrascoffee.com/"; // Replace with your actual home page URL
+
+  // Check if the current URL is the same as the home page URL
+  if (currentUrl === homePageUrl) {
+    $.scrollify({
+      section: ".scroll-section",
+      interstitialSection: "",
+      scrollSpeed: 600,
+      offset: 0,
+      scrollbars: true,
+      standardScrollElements: "",
+      setHeights: true,
+      overflowScroll: true,
+      updateHash: false,
+      touchScroll: true,
+      before: function () {},
+      after: function () {},
+      afterResize: function () {},
+      afterRender: function () {},
+    });
+
+    var controller = new ScrollMagic.Controller();
+
+    // build scene
+    var scene = new ScrollMagic.Scene({
+      triggerHook: 1,
+      triggerElement: "#fixed-target",
+      pushFollowers: false,
+    })
+      .setPin("#fixed-footer")
+      .addTo(controller);
+  }
+
   // document end
 });
