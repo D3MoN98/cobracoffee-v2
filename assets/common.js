e1671ba12a50783f2668b0648fdd7497c8128699
Sudipta Jana <sjgalaxy98@gmail.com>
2023-10-04 23:44:24 +0530

    page modification

--- assets/common.js
+++ assets/common.js
@@ -176,7 +176,7 @@ jQuery(document).ready(function ($) {
   // Define the URL of your home page
   var homePageUrl = "https://cobrascoffee.com/"; // Replace with your actual home page URL
 
-  // var controller = new ScrollMagic.Controller();
+  var controller = new ScrollMagic.Controller();
 
   // // build scene
   var scene = new ScrollMagic.Scene({
