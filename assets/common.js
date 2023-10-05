1c31c362a6a842e7409f95ab34eec57acf1bd01f
Sudipta Jana <sjgalaxy98@gmail.com>
2023-10-05 18:07:18 +0530

    page modification

--- assets/common.js
+++ assets/common.js
@@ -76,13 +76,13 @@ jQuery(document).ready(function ($) {
 
   /*testimonial*/
   $(".product-slider").slick({
-    speed: 3000,
+    speed: 500,
     autoplay: true,
     pauseOnHover: true,
     autoplaySpeed: 0,
     cssEase: "linear",
     slidesToShow: 3,
-    slidesToScroll: 0.1,
+    slidesToScroll: 0.5,
     infinite: true,
     centerMode: true,
     centerPadding: "250px",
