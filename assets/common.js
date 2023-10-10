7f0cc81f998eb9b372bee42fd4d835d770b56e72
Sudipta Jana <sjgalaxy98@gmail.com>
2023-10-10 18:53:36 +0530

    page modification

--- assets/common.js
+++ assets/common.js
@@ -189,6 +189,12 @@ jQuery(document).ready(function ($) {
   const scroller = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
     smooth: true,
+    smartphone: {
+      smooth: true,
+    },
+    tablet: {
+      smooth: true,
+    },
   });
 
   //responsive tabs
